deca09ec8eb5a7e09f8e974aab7d40796656bbc3
Matheus Andrade <matheusroberto.alvesandrade1@gmail.com>
2025-10-01 23:13:29 -0300

    Ajustando código para parsear por categorias

--- app/services/extract.py
+++ app/services/extract.py
@@ -1,16 +1,15 @@
-from app.utils.functions import page_request, bs4_parser
+from app.utils.functions import page_request, bs4_obj, html_parser, page_amount
 
-if __name__=='__main__':
-    base_url = "https://books.toscrape.com/"
-    page_url = "https://books.toscrape.com/catalogue/page-{}.html"
+base_url = "https://books.toscrape.com/"
+page_url = "https://books.toscrape.com/catalogue/page-{}.html"
 
-    response = page_request(base_url)
+response = page_request(base_url)
 
-    soup = bs4_parser(response.text)
+soup = bs4_obj(response.text)
 
-    categorias = soup.find('div', class_='side_categories')
+categorias = soup.find('div', class_='side_categories')
 
-    lista_categorias = []
+lista_categorias = []
 
 for li in categorias.find('ul').find('li').find('ul').find_all('li'):
     categoria = li.get_text(strip=True)
@@ -22,5 +21,22 @@ for li in categorias.find('ul').find('li').find('ul').find_all('li'):
 
     lista_categorias.append(dicionario)
 
+for link in lista_categorias['link'] and lista_categorias['categoria']:
 
-print(lista_categorias)
+    request = page_request(link)
+
+    soup = bs4_obj(request.text)
+
+    page_amt = page_amount(link)
+
+    if page_amt==1:
+        livros = html_parser(request.text)
+    else:
+        for page in range(1, page_amt+1):
+            response = page_request(f'page-{page}.html')
+            if response.status_code != 200:
+                print(f"Erro na página: {page}")
+                continue
+
+            soup = bs4_obj(response.text)
+            livros = html_parser(soup)        
