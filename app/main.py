58ae8a785bb75224546fad17b6bb43a7a672dfc5
Matheus Andrade <matheusroberto.alvesandrade1@gmail.com>
2025-11-01 15:33:54 -0300

    adicionando serviço de estatisticas

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from app.core.start_up.bootstrap import init
 from app.api.routers import auth_controller
 from app.api.routers import user_controller
 from app.api.routers import books_controller
+from app.api.routers import stats_controller
 from fastapi import FastAPI
 
 init()
@@ -16,6 +17,7 @@ app = FastAPI(
 app.include_router(auth_controller.router)
 app.include_router(user_controller.router)
 app.include_router(books_controller.router)
+app.include_router(stats_controller.router)
 
 @app.on_event("shutdown")
 async def shutdown():
